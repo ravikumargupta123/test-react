6d116ae81fc4f38bfa51dcd662372e0707106c08
ravi gupta <ravi.gupta@happay.in>
2019-05-27 18:38:48 +0530

    +

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactaskadjlka
         </a>
       </header>
     </div>
